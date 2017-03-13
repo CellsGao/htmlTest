5bf8f10045a0a5a9baf6fa1419ab6cba88758584
Cellsgao <326422808@qq.com>
2017-03-13 22:37:45 +0800

    第八章 显示略缩语列表

---
+++ scripts/displayAbbreviations.js
@@ -0,0 +1,37 @@
+/**
+ * Created by gaoshang on 2017/3/13.
+ */
+
+//函数介绍位于js DOM编程艺术第八章133页。
+function displayAbbreviation() {
+    var abbreviations=document.getElementsByTagName("abbr");
+    if(abbreviations.length<1){
+        return false;
+    }
+    var defs=[];
+    for(var i=0;i<abbreviations.length;i++){
+        var definition=abbreviations[i].getAttribute("title");
+        var key=abbreviations[i].lastChild.nodeValue;
+        defs[key]=definition;
+    }
+    var dlist=document.createElement("dl");
+    for (key in defs){
+        var definition1=defs[key];
+        var dtitle=document.createElement("dt");
+        var dtitle_text=document.createTextNode(key);
+        dtitle.appendChild(dtitle_text);
+        var ddesc=document.createElement("dd");
+        var ddesc_text=document.createTextNode(definition1);
+        ddesc.appendChild(ddesc_text);
+        dlist.appendChild(dtitle);
+        dlist.appendChild(ddesc);
+    }
+    var header=document.createElement("h2");
+    var header_text=document.createTextNode("Abbreviations");
+    header.appendChild(header_text);
+    document.body.appendChild(header);
+    document.body.appendChild(dlist);
+}
+//如果引用了addLoadEvent函数，则html文件中addLoadEvent的scrip标签必须在其之前，否则无效。
+addLoadEvent(displayAbbreviation());
+
